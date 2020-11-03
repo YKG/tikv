49aabf091cc904a6312fa9363c4c2efd89bf573f
YKG <yekaige2010@gmail.com>
2020-11-03 22:53:48 +0800

    update

--- src/server/service/kv.rs
+++ src/server/service/kv.rs
@@ -126,11 +126,11 @@ macro_rules! handle_request {
                 return;
             }
             let timer = GRPC_MSG_HISTOGRAM_VEC.$fn_name.start_coarse_timer();
-            let start = Instant::now();
+            let start2 = Instant::now();
             let future = $future_name(&self.storage, req)
                 .and_then(|res| sink.success(res).map_err(Error::from))
-                .map(|start| {
-                    debug!("YKGX handle_request "; "fn_name"=> stringify!($fn_name), "diff" => start.elapsed().as_millis());
+                .map(|start2| {
+                    debug!("YKGX handle_request fn_name: {:?} diff: {:?}", stringify!($fn_name), start2.elapsed().as_millis());
                     timer.observe_duration()
                 })
                 .map_err(move |e| {
